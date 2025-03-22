e41d245116b1dc0a8216ffd277529a61c39d8150
Victor Augusto <163042675+VryDeveloper@users.noreply.github.com>
2025-03-22 05:35:03 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -251,7 +251,7 @@ function tocarSom(audio) {
 document.getElementById('btnFlintAndSteal').addEventListener('click', () => tocarSom('./assets/sounds/Flint_and_steel_click.ogg'));
 document.getElementById('btnScissors').addEventListener('click', () => tocarSom('./assets/sounds/Shear.ogg'));
 document.getElementById('btnPaper').addEventListener('click', () => tocarSom('./assets/sounds/Page_turn1.ogg'));
-document.getElementById('btnRock').addEventListener('click', () => tocarSom('./assets/sounds/Calcite_break1.ogg'));
+document.getElementById('btnRock').addEventListener('click', () => tocarSom('../assets/sounds/Calcite_break1.ogg'));
 document.getElementById('btn-shop1').addEventListener('click', () => tocarSom('./assets/sounds/Pop.ogg'));
 document.getElementById('btn-shop2').addEventListener('click', () => tocarSom('./assets/sounds/Pop.ogg'));
 document.getElementById('btn-shop3').addEventListener('click', () => tocarSom('./assets/sounds/Pop.ogg'));
